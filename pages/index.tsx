c6ed3ad8a37003c0a062c8129c496e0ff9c2c7ef
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-05-24 12:06:54 +0530

    title change

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className=" ">
       <Head>
-        <title>Create Next App</title>
+        <title>Tesla App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
